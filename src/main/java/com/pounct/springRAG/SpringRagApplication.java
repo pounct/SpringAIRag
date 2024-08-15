48eec240b6c4e2c55207f3fff3991594c7afc824
Pounct <53088375+pounct@users.noreply.github.com>
2024-08-15 22:07:33 +0200

    Add files via upload

---
+++ src/main/java/com/pounct/springRAG/SpringRagApplication.java
@@ -0,0 +1,13 @@
+package com.pounct.springRAG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pounct/springRAG/SpringRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pounct.springRAG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
